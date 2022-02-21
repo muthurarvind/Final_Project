dd8979901da7c97c9796c202a433edb5fc613a59
muthurarvind <94586791+muthurarvind@users.noreply.github.com>
2022-02-21 10:18:25 +0530

    Update stack.tf

--- stack.tf
+++ stack.tf
@@ -169,7 +169,7 @@ Resources:
             sudo /opt/lampp/lampp restart
             sudo apt install mysql-client-core-8.0
             sudo mysql -h ${aws_cloudformation_stack.network.template_body.${!GetAtt RDSDBI32QQE.Endpoint.Address}} -u admin --password=ramana4u2021 buspassdb < /Final_Project/code/SQL\ File/buspassdb.sql
-          - endpoint: aws_cloudformation_stack.network.template_body.${!GetAtt RDSDBI32QQE.Endpoint.Address
+          - endpoint: aws_cloudformation_stack.network.template_body.${!GetAtt RDSDBI32QQE.Endpoint.Address}
     Metadata:
       'AWS::CloudFormation::Designer':
         id: 355e45e0-9d36-49a7-8d8d-fc3b7f760b9c
